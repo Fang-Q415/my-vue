93fc909e50bd98fd2fef5715736e2fd239a487ea
Fang-Q415 <17538135590@163.com>
2021-03-08 17:29:40 +0800

    项目结构划分和路由

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="app">
+    <main-tab-bar></main-tab-bar>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-
+  import MainTabBar from "components/content/mainTabbar/MainTabBar"
   export default {
     name: 'app',
     components: {
-      
+      MainTabBar
     }
   }
 </script>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+home
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
